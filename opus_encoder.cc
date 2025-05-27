f0e51d1bb1ac7f37241bb6577bca206abae9c049
Terrence <terrence@tenclass.com>
2025-05-28 07:30:58 +0800

    Bump to 2.3.3

--- opus_encoder.cc
+++ opus_encoder.cc
@@ -37,6 +37,8 @@ void OpusEncoderWrapper::Encode(std::vector<int16_t>&& pcm, std::function<void(s
     if (in_buffer_.empty()) {
         in_buffer_ = std::move(pcm);
     } else {
+        /* ISSUE: https://github.com/78/esp-opus-encoder/issues/1 */
+        in_buffer_.reserve(in_buffer_.size() + pcm.size());
         in_buffer_.insert(in_buffer_.end(), pcm.begin(), pcm.end());
     }
 
